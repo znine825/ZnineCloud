32f8e09aa272b6eafc1d004853842eee9084416b
ZNINE <earthonemen@gmail.com>
2026-03-23 03:57:53 +0900

    Page2 add

--- src/MHome/MHome.jsx
+++ src/MHome/MHome.jsx
@@ -1,5 +1,6 @@
 import './MHome.css'
 import './MHomePage2.css'
+import './MHomePage3.css'
 import Header from './../Components/Header/Header.jsx'
 import { Link } from "react-router-dom";
 
@@ -7,9 +8,14 @@ function Page2_Service(props) {
 
     return (
         <div className = 'PS_Main'>
-            <p className = 'PS_p PS_Num'>{ props.Num }</p>
-            <p className = 'PS_p PS_Title'>{ props.Title }</p>
-            <p className = 'PS_p PS_SubTitle'>{ props.SubTitle }</p>
+            <p className = 'PS_Num'>{ props.Num }</p>
+            <div>
+                <div>
+                    <p className = 'PS_Title'>{ props.Title }</p>
+                    <p className = 'PS_SubTitle'>{ props.SubTitle }</p>
+                </div>
+                <p className = 'PS_Text'>{ props.Text }</p>
+            </div>
         </div>
     )
 }
@@ -57,20 +63,24 @@ function MHome() {
             <div className = 'MH2_Page2'>
                 <p className = 'MH2_Title'>Services</p>
                 <p className = 'MH2_SubTitle'>YOU CAN DO</p>
+                <div className = 'MH_Services'>
+                    <div className = 'MH2_GuideLine'></div>
+                    <Page2_Service Num = '01' Title = 'Learn' SubTitle = 'Programming Lecture' Text = '다양한 프로그래밍 언어를 배울 수 있습니다.'/>            
+                    <div className = 'MH2_GuideLine'></div>
+                    <Page2_Service Num = '02' Title = 'Experience' SubTitle = 'Technology Experience' Text = '간단한 알고리즘을 이용한 기술을 체험 할 수 있습니다.'/>            
+                    <div className = 'MH2_GuideLine'></div>
+                    <Page2_Service Num = '03' Title = 'Tools' SubTitle = 'Useful features' Text = '개발하는 데 유용한 몇 가지 도구를 사용할 수 있습니다.'/>            
+                    <div className = 'MH2_GuideLine'></div>
+                    <Page2_Service Num = '04' Title = 'Docs' SubTitle = 'documents' Text = '사이드에 대한 정보가 담긴 문서를 읽을 수 있습니다.'/>            
+                    <div className = 'MH2_GuideLine'></div>
+                </div>
             </div>
-            <div className = 'MH2_GuideLine GL1'></div>
-            <div className = 'PS_1'>
-                <Page2_Service Num = '01' Title = 'Learn' SubTitle = 'Programming Lecture'/>            
+
+            {/* ====================PAGE 3================== */}
+
+            <div className = 'MH3_Page3'>
+
             </div>
-            <div className = 'MH2_GuideLine GL2'></div>
-            <div className = 'PS_2'>
-                <Page2_Service Num = '02' Title = 'Experience' SubTitle = 'Technology Experience'/>            
-            </div>
-            <div className = 'MH2_GuideLine GL3'></div>
-            <div className = 'PS_3'>
-                <Page2_Service Num = '03' Title = 'Tools' SubTitle = 'Useful features'/>            
-            </div>
-            <div className = 'MH2_GuideLine GL4'></div>
             
         </div>
     )
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: '/',
 })
